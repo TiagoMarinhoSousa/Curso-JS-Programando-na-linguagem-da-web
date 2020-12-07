e2cc1a0dd5a2e9d418762b76c7f2710ea3b18517
Tiago Marinho Sousa <tiagomarinho9101@gmail.com>
2020-12-07 18:58:53 -0300

    aula 3 - laço for

--- js/principal.js
+++ js/principal.js
@@ -3,35 +3,41 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 //2ª aula - 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
 
-var tdImc = paciente.querySelector(".info-imc");
+for (var i = 0; i < pacientes.length; i++) {
 
-//validação do peso e altura
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var paciente = pacientes[i];
 
-if(peso <= 0 || peso >= 300){
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido!";
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    //validação do peso e altura
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 300) {
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
 }
-
-if(altura <= 0 || altura >= 3.00){
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida!";
-}
-
-if (pesoEhValido && alturaEhValida){
-    var imc = peso / (altura*altura);
-    tdImc.textContent = imc;
-}
-
-
-
-
